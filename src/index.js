2ac991d7143377f84757cbe29b5e227fe305c6d2
haristotle <haristotlepatil2003@gmail.com>
2025-06-22 02:17:34 +0530

    Edit html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import "./style.css";
 import createHome from "./modules/home.js";
 import createMenu from "./modules/menu.js";
-import createContact from "./modules/contact.js"
+import createContact from "./modules/contact.js";
 
-createContact();
+const content = document.querySelector("#content");
+const homeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const contactButton = document.querySelector("#contact-us");
+
+homeButton.addEventListener("click", () => {
+  content.removeChild(document.querySelector(".actual-content"));
+  createHome();
+  return;
+});
+
+contactButton.addEventListener("click", () => {
+  content.removeChild(document.querySelector(".actual-content"));
+  createContact();
+  return;
+});
+
+menuButton.addEventListener("click", () => {
+  content.removeChild(document.querySelector(".actual-content"));
+  createMenu();
+  return;
+});
+
+createHome();
