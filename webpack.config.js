1aa00b5026b06b12ee3ffa347c439367368395b8
haristotle <haristotlepatil2003@gmail.com>
2025-06-21 22:49:38 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { loadEnvFile } = require("process");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpeg|svg|jpg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
